fd2abb79172617c099b3ee410da59b4000d6ce4e
saraphina1 <oyeyemibunmi21@gmail.com>
2023-11-20 14:54:59 +0100

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,39 +13,39 @@ import {
 import { CSS } from "@dnd-kit/utilities";
 
 
-const SortableUser = ({ item }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: item.id });
+// const SortableUser = ({ item }) => {
+//   const { attributes, listeners, setNodeRef, transform, transition } =
+//     useSortable({ id: item.id });
 
-  const style = {
-    transition,
-    transform: CSS.Transform.toString(transform),
-  };
-  function handleClick(){
-    localStorage.setItem("isLogged" , true)
-    window.location.href="/"
-  }
-  return (
-    <div
-    onClick={()=>handleClick()}
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      key={item.id}
-      className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
-      <div className="card p-0 overflow-hidden h-100 shadow m-2">
-<img src={item.image} className="card-img-top" alt="img"/>
-<div className="card-body">
-<h5 className="card-title">{item.tag}</h5>
-</div>
-</div>
+//   const style = {
+//     transition,
+//     transform: CSS.Transform.toString(transform),
+//   };
+//   function handleClick(){
+//     localStorage.setItem("isLogged" , true)
+//     window.location.href="/"
+//   }
+//   return (
+//     <div
+//     onClick={()=>handleClick()}
+//       ref={setNodeRef}
+//       style={style}
+//       {...attributes}
+//       {...listeners}
+//       key={item.id}
+//       className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+//       <div className="card p-0 overflow-hidden h-100 shadow m-2">
+// <img src={item.image} className="card-img-top" alt="img"/>
+// <div className="card-body">
+// <h5 className="card-title">{item.tag}</h5>
+// </div>
+// </div>
 
 
 
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
 function Home() {
   const [filter, setFilter] = useState("");
